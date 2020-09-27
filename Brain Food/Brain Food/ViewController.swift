48783fff0251e5c9aafcd451ccbc9dbc0dacb6de
Thomas Sabino-Benowitz <53605757+RoastBeefKazenakis@users.noreply.github.com>
2020-09-26 22:35:29 -0400

    Base Project

---
+++ Brain Food/Brain Food/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Brain Food
+//
+//  Created by Thomas Sabino-Benowitz on 9/26/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
